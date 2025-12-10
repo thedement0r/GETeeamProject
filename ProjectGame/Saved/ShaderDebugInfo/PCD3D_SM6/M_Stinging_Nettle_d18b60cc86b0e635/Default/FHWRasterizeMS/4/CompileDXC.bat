148bb00c8e43ea0f645e2ef9c631210c7330de48
Moroz Dmytro <aboutmorozdmytro@gmail.com>
2025-12-10 15:39:22 +0100

    bad_export
    Bad export attempt (need to discuss which way we go)

---
+++ ProjectGame/Saved/ShaderDebugInfo/PCD3D_SM6/M_Stinging_Nettle_d18b60cc86b0e635/Default/FHWRasterizeMS/4/CompileDXC.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET DXC="D:\UE_5.4\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+IF NOT EXIST %DXC% (
+	ECHO Couldn't find dxc.exe under "D:\UE_5.4\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	GOTO :END
+)
+%DXC% -HV 2021 -Zpr -O3 -auto-binding-space 0 -Wno-parentheses-equality -disable-lifetime-markers -T ms_6_6 -E HWRasterizeMS -Fc NaniteRasterizer.d3dasm -Fo NaniteRasterizer.dxil NaniteRasterizer.usf
+:END
+PAUSE
